1cff000498cf1cf4e81ba8ded10bf400c43b92fd
pavelsannikov <pavelsan95@mail.ru>
2022-10-09 09:59:58 +0500

    Добавьте файлы проекта.

---
+++ Module10/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            double a = Double.Parse(Console.ReadLine());
+            double b = Double.Parse(Console.ReadLine());
+            Math math = new Math();
+            Console.WriteLine(math.Sum(a, b));
+        }
+        catch(System.FormatException)
+        {
+            Console.WriteLine("Недопустимые значения");
+        }
+    }
+    public interface IMath
+    {
+        public double Sum(double a, double b);
+    }
+    class Math : IMath
+    {
+        public double Sum(double a, double b) { return a + b; }
+    }
+}
---
+++ Module10_2/Program.cs
@@ -0,0 +1,57 @@
+﻿class Program
+{
+    static ILogger math_log;
+    static void Main(string[] args)
+    {
+        math_log = new Logger();
+        Math math = new Math(math_log);
+        math.Sum();
+    }
+    
+    public interface ILogger
+    {
+        void Event(string msg);
+        void Error(string msg);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string msg)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(msg);
+        }
+
+        public void Event(string msg)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(msg);
+        }
+    }
+    public interface IMath
+    {
+        void Sum();
+    }
+    class Math : IMath
+    {
+        ILogger math_log;
+        public Math(ILogger _log)
+        {
+            math_log = _log;
+        }
+        public void Sum() {
+            try
+            {
+                double a = Double.Parse(Console.ReadLine());
+                double b = Double.Parse(Console.ReadLine());
+                Console.WriteLine((a + b).ToString());
+                math_log.Event((a + b).ToString());
+            }
+            catch (System.FormatException)
+            {
+                math_log.Error("Недопустимые значения");
+            }
+        }
+    }
+}
